c47d647e494922629757a4d569db129d603b8e11
Francesco Novy <francesco@cropster.com>
2016-06-15 08:35:25 -0700

    Replace L10n.reopen() in initializer with an extended service
    The extended service is added in the default blueprint

--- blueprints/ember-l10n-initializer/files/__root__/initializers/__name__.js
+++ blueprints/ember-l10n-initializer/files/__root__/initializers/__name__.js
@@ -1,29 +1,10 @@
-//import L10N from 'ember-l10n/services/l10n';
-
-export function initialize(/*application*/) {
-
-  // Configure L10N service options before instantiating
-  /*
-  L10N.reopen({
-      // specify all your available languages generated with gettext.sh
-      availableLocales: Ember.computed(function(){
-        return {
-            'en': this.t('en'),
-        };
-      }),
-      jsonPath: '/custom/path/to/json/files', // provide different location of JSON files
-      autoInitialize: false, // no auto detection, triggered only when calling setLocale() manually
-      forceLocale: 'de', // skips language detection, only useful if `autoInitialize:true` (= default)
-  });
-  */
-
+export function initialize(application) {
   // Inject the service into your desired object types
-  /*
   application.inject('model', 'l10n', 'service:l10n');
   application.inject('route', 'l10n', 'service:l10n');
   application.inject('controller', 'l10n', 'service:l10n');
   application.inject('component', 'l10n', 'service:l10n');
-  */
+
 }
 
 export default {
