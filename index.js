2b2dd268b9cbdc86acc0ab2433929d7cff22e860
Armin Pfurtscheller <armin.pfurtscheller@gmail.com>
2016-07-19 10:53:25 +0200

    Improved and finished refactor from BASH to Node.js fixes #4

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ module.exports = {
 
   includedCommands: function() {
     return {
+      'l10n:install': require('./lib/commands/install'),
       'l10n:extract': require('./lib/commands/extract'),
-      'l10n:convert': require('./lib/commands/convert')
+      'l10n:convert': require('./lib/commands/convert'),
+      'l10n:sync': require('./lib/commands/sync')
     };
   },
 
