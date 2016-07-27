886deb1935b4baa6a9d2fe0e7b00e1925ccd910f
Francesco Novy <francesco@cropster.com>
2016-07-27 10:31:42 +0200

    Add fallback to app.import if this.import is not available

--- index.js
+++ index.js
@@ -22,9 +22,12 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
+    // In nested addons, app.bowerDirectory might not be available
     var bowerDirectory = app.bowerDirectory || 'bower_components';
+    // In ember-cli < 2.7, this.import is not available, so fall back to use app.import
+    var importShim = typeof this.import !== 'undefined' ? this : app;
 
-    this.import(bowerDirectory + '/gettext.js/dist/gettext.min.js', {
+    importShim.import(bowerDirectory + '/gettext.js/dist/gettext.min.js', {
       exports: {
         'i18n': [
           'default'
